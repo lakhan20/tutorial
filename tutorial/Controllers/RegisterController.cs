4b6828806cedee533c078e4077017f131424616a
lakhan20 <lakhanrthakkar22@gmail.com>
2024-03-19 10:33:25 +0530

    Add project files.

---
+++ tutorial/Controllers/RegisterController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Npgsql;
+using System.Configuration;
+using tutorial.Models;
+namespace tutorial.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // GET: Register
+
+        myClass obj = new myClass();
+        public ActionResult register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult insertData(string fullname,string username, string password)
+        {
+            Console.WriteLine("<script> console.log('Inside insertData');</script>");
+            List<string> lst=new List<string>();
+            using(NpgsqlConnection connection=new NpgsqlConnection())
+            {
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["con"].ToString();
+                connection.Open();
+                string query = "";
+                query = "INSERT INTO employee (fullname,username,password) VALUES ('" + fullname.Trim().ToString() + "'," + " '" + username.Trim().ToString() + "'," + " '" + password.Trim().ToString() + "' )";
+                NpgsqlCommand cmd = new NpgsqlCommand(query,connection);
+                cmd.Connection = connection;
+                cmd.ExecuteNonQuery();
+                lst.Add("Data Inserted.");
+
+
+            }
+            return Json(lst);
+        }
+    }
+}
---
+++ tutorial/Models/myClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace tutorial.Models
+{
+    public class myClass
+    {
+        public string encryptstring(string val)
+        {
+
+            byte[] b= System.Text.ASCIIEncoding.ASCII.GetBytes(val);
+            string enval=Convert.ToBase64String(b);
+            return enval;
+
+
+        }
+    }
+}
